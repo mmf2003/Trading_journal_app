06fb9f815c73dc35e13c7f2196efb5498d26999f
Sidelnikov Oleksandr <mmf2003@ukr.netr>
2026-07-23 16:34:04 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+    return (
+        <main>
+            <h1>Trading Journal</h1>
+            <p>Professional trading analytics platform</p>
+        </main>
+    );
+}
+
+export default App;
